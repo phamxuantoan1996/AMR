f6cce7f5151ae396715a8c00aca445ddb1fba8c9
phamxuantoan1996 <phamxuantoandc@gmail.com>
2026-01-04 12:52:03 +0700

    feat : Add Mission_Active_Info_Structure,  AMR_State_Structure struct type.

--- common/common.h
+++ common/common.h
@@ -1,6 +1,14 @@
 #ifndef _COMMON_H
 #define _COMMON_H
 #include <stdint.h>
+#include <cstdint>
+#include <semaphore.h>
+#include <string>
+
+#define SHM_AMR_STATE_NAME "/shm_amr_status"
+constexpr size_t AMR_STATUS_MAX_PAYLOAD = 1024 * 5; //
+constexpr size_t SEER_STATE_SIZE = 1024*3;
+constexpr uint64_t SHM_AMR_STATE_MAGIC = 0xA5A5A5A5A5A5A5A5ULL;
 
 #define MISSION_BUFFER_SIZE 512
 
@@ -10,6 +18,10 @@
 
 enum MISSION_CONFIRM_TYPE {
     MISSION_CONFIRM_NONE,MISSION_CONFIRM_RUN,MISSION_CONFIRM_COMPLETE,MISSION_CONFIRM_CANCEL,
+};
+
+enum ROBOT_STATE {
+    ROBOT_STATE_NONE,ROBOT_STATE_IDLE,ROBOT_STATE_ACTIVE,ROBOT_STATE_STOP,ROBOT_STATE_ERROR
 };
 
 enum MISSION_STATE {
@@ -66,13 +78,31 @@ struct Mission_Control_Signal_Structure
     bool signal_cancel;
 };
 
-
 struct AMR_Signal_Control_Structure
 {
     /* data */
     AMR_Relocation_Data_Structure signal_relocation;
     LIFT_LEVEL signal_lift;
     Mission_Control_Signal_Structure signal_mission;
+};
+
+struct Mission_Active_Info_Structure
+{
+    /* data */
+    int32_t mission_code;
+    int32_t action_index;
+    MISSION_STATE mission_state;
+    MISSION_ERROR_CODE mission_error_code;
+};
+
+struct AMR_State_Structure
+{
+    /* data */
+    char seer_state[SEER_STATE_SIZE];
+    COMMODITY_STATE commodity_state;
+    LIFT_LEVEL lift_state;
+    ROBOT_STATE robot_state;
+    Mission_Active_Info_Structure mission_active_info;
 };
 
 
